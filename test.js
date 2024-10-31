f4801376c442f94fad407806627486741199fd13
user-irfan <kuttappuirfan244@gmail.com>
2024-10-31 10:48:40 +0530

    singleNonDuplicate

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
-let arr=[-1,0]
-
-function single(arr=[]){
-   let result=[]
-   for (let i=0;i<arr.length;i++){
-      if(arr.indexOf(arr[i])==arr.lastIndexOf(arr[i])){
-         result.push(arr[i])
-      }
+var singleNonDuplicate = function(nums) {
+   for(let i=0;i<nums.length;i++){
+    if(nums[i]!==nums[i+1] && nums[i-1]!==nums[i]){
+        return nums[i]
+    }
    }
-   return result
-}
+};
 
-console.log(single(arr));
+console.log(singleNonDuplicate([3,3,7,7,10,11,11]));
