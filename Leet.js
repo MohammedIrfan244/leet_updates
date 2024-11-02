d30f3c7990396af7b267402249161daa498f9429
user-irfan <kuttappuirfan244@gmail.com>
2024-11-02 10:09:41 +0530

    isIsomorphic

--- Leet.js
+++ Leet.js
@@ -1,15 +1,9 @@
-var isCircularSentence = function(sentence) {
-    let strArr=sentence.split(" ")
-    if (strArr.length ===1){
-        return sentence[0]===sentence[sentence.length-1];
-    }
-    
-    for (let i=0;i<strArr.length-1;i++){
-        if (strArr[i][strArr[i].length-1]!==strArr[i+1][0]){
-            return false;
-        }
-    }
-    return strArr[strArr.length-1][strArr[strArr.length-1].length-1]===strArr[0][0];
+var isIsomorphic = function(s="", t="") {
+    let sArr=s.split("")
+    let tArr=t.split("")
+   let sStr= sArr.map((item,_,arr)=>arr.lastIndexOf(item)).join("")
+   let tStr= tArr.map((item,_,arr)=>arr.lastIndexOf(item)).join("")
+   return sStr==tStr
 };
 
-console.log(isCircularSentence("a a ba"));
+console.log(isIsomorphic("abcdefghijklmnopqrstuvwxyzva","abcdefghijklmnopqrstuvwxyzck"));
