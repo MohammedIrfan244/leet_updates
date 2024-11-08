2657891ab6295f1eb454f89c31b24003260b3dfb
user-irfan <kuttappuirfan244@gmail.com>
2024-11-08 08:51:32 +0530

    sortBy

--- Leet.js
+++ Leet.js
@@ -1,11 +1,5 @@
-var searchInsert = function(nums=[], target=0) {
-    if(nums.includes(target)){
-        return nums.indexOf(target)
-    }else{
-        nums.push(target)
-        nums.sort((a,b)=>a-b)
-        return nums.indexOf(target)
-    }
+var sortBy = function(arr=[], fn) {
+    return arr.sort((a,b)=>fn(a)-fn(b))
 };
 
-console.log(searchInsert([1,3,5,6],7))
+console.log(sortBy([5, 4, 1, 2, 3], fn = (x) => x))
