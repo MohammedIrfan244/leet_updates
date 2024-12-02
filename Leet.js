34eb8940e245831104ce1abd59fafe5562d18f61
user-irfan <kuttappuirfan244@gmail.com>
2024-12-02 12:41:50 +0530

    majorityElement

--- Leet.js
+++ Leet.js
@@ -1,6 +1,17 @@
-var reverseWords = function(s="") {
-    let arr=s.split(" ").filter(i=>i.trim()!="")
-    return arr.reverse().join(" ")
+var majorityElement = function(nums=[]) {
+    
+    for (let i=0;i<nums.length;i++){
+        let occ=0
+        for(let k=0;k<nums.length;k++){
+            if(nums[i]==nums[k]){
+                occ+=1
+            }   
+            if(occ>=nums.length/2){
+            return nums[i]
+           }
+        }
+        
+    }
 };
 
-console.log(reverseWords("a good   example"))
+console.log(majorityElement([2,2,1,1,1,2,2]))
