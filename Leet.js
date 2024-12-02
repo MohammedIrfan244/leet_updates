1f7d526c1d89fe21f076c49ebc617a760d05fb55
user-irfan <kuttappuirfan244@gmail.com>
2024-12-02 11:27:13 +0530

    reverseWords

--- Leet.js
+++ Leet.js
@@ -1,11 +1,6 @@
-var isPrefixOfWord = function(sentence="", searchWord="") {
-    const wordArr=sentence.split(" ")
-    for(let i=0;i<wordArr.length;i++){
-        if(wordArr[i].startsWith(searchWord)){
-            return i+1
-        }
-    }
-    return -1
+var reverseWords = function(s="") {
+    let arr=s.split(" ").filter(i=>i.trim()!="")
+    return arr.reverse().join(" ")
 };
 
-console.log(isPrefixOfWord("i love eating burger","burg"))
+console.log(reverseWords("a good   example"))
