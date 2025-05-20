3073449f6474e77108edd0eb2caf094030c3bad5
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-05-20 14:10:48 +0530

    isZeroArray

--- Leet.js
+++ Leet.js
@@ -1,5 +1,13 @@
-var isAnagram = function(s, t) {
-    return s.split("").sort().join()==t.split("").sort().join()
+var isZeroArray = function(nums, q) {
+   let refArr = Array.from({length:nums.length+1}).fill(0)
+   for([l,r] of q){
+       refArr[l] +=1
+       refArr[r+1] -=1
+   }
+   let acc=0
+   for ( let i =0 ;i<nums.length;i++){
+       acc+=refArr[i]
+       if(acc<nums[i]) return false
+   }
+   return true
 };
-
-console.log(isAnagram("rat","car"))
